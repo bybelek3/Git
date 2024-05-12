8b2221892c53999395418a17764167633bf6c1a8
bybelek3 <169060920+bybelek3@users.noreply.github.com>
2024-05-12 21:57:10 +0200

    Update Note.java

--- src/main/java/Note.java
+++ src/main/java/Note.java
@@ -1,2 +1,32 @@
-public class Note {
+public class NotesManager {
+
+    public String getNotesInAFormattedWay()
+    {
+        String formattedText = "";
+        for (Note note : Main.notes)
+        {
+            formattedText += note.toString() + "\n";
+        }
+        return formattedText;
+    }
+
+    public void deleteNote(String id)
+    {
+        boolean isExist = false;
+        for (Note note : Main.notes)
+        {
+            if (note.getId().equals(id))
+            {
+                Main.notes.remove(note);
+                isExist = true;
+                System.out.println("You deleted the note with id: "
+                        + note.getId());
+                break;
+            }
+        }
+        if (!isExist)
+        {
+            System.out.println("Id does not exist");
+        }
+    }
 }
